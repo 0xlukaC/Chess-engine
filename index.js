147775cea756a2d11d2834555ae798dc1ab336ce
luka.chalker <52194915+cyber-chalk@users.noreply.github.com>
2023-06-07 03:39:34 +0000

    began frontend

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-// const express = require("express");
-// const app = express();
+const express = require("express");
+const app = express();
 
-// app.use(express.static("public")); // uses the public folder for static files
+app.use(express.static("public")); // uses the public folder for static files
 
-// app.listen(3000);
+app.listen(3000);
 
 const { Chess } = require("chess.js");
 const chess = new Chess(); // start the game
@@ -337,8 +337,4 @@ function avengersEndgame(endgameWeight) {
 }
 
 // console.log(evaluate(chess.board()));
-move();
-chess.move("d4");
-move();
-chess.move("Nf3");
-move();
+// move();
---
+++ public/board.js
@@ -0,0 +1,1 @@
+let board = Chessboard("myBoard", "start");
